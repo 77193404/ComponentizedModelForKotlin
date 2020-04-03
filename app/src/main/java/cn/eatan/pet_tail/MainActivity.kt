03c81e55305e71f9d554475ef35f43c5ab9cccb6
eatan96 <77193404@qq.com>
2020-04-03 18:09:56 +0800

    组件化模型

--- app/src/main/java/cn/eatan/pet_tail/MainActivity.kt
+++ app/src/main/java/cn/eatan/pet_tail/MainActivity.kt
@@ -5,23 +5,39 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.widget.TextView
-import cn.eatan.module_user.ui.activity.LoginModeActivity
+import cn.eatan.lib_base.base.constant.Constant.Companion.ROUTER_LOGIN_ACTIVITY
 import cn.eatan.utils.statusbarutil.StatusBarUtil
+import com.alibaba.android.arouter.launcher.ARouter
 import kotlinx.android.synthetic.main.activity_main.*
-import org.jetbrains.anko.startActivity
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.debug
+import org.jetbrains.anko.info
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), AnkoLogger {
 
+    private lateinit var myCountDownTimer: MyCountDownTimer
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         StatusBarUtil.transparencyBar(this, false)
-        MyCountDownTimer(this, countdownTv).start()
+        myCountDownTimer = MyCountDownTimer()
     }
 
-    private class MyCountDownTimer(internal var content : Context, internal var countdownTv: TextView) :CountDownTimer(3000, 1000){
+    override fun onResume() {
+        super.onResume()
+        myCountDownTimer.start()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        myCountDownTimer.cancel()
+    }
+
+
+
+    inner class MyCountDownTimer:CountDownTimer(3000, 1000){
         override fun onFinish() {
-            content.startActivity<LoginModeActivity>()
+            ARouter.getInstance().build(ROUTER_LOGIN_ACTIVITY).navigation()
         }
 
         override fun onTick(p0: Long) {
